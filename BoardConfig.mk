8655e5a1d5f863f944deea706f38578bd58164bd
sunflowers.warp@gmail.com <sunflowers.warp@gmail.com>
2026-07-21 20:17:23 +0300

    avb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,8 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/meizu/m1822
 TARGET_KERNEL_VERSION := 4.9
 
+BOARD_AVB_ENABLE := true
+
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
