551c20d9fa6c86898b02d05fef05e91d60318416
sunflowers.warp@gmail.com <sunflowers.warp@gmail.com>
2026-07-22 23:41:56 +0300

    m1822: Fix malformed COPY_FILE error in device.mk

--- device.mk
+++ device.mk
@@ -193,14 +193,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1 \
     android.hardware.biometrics.fingerprint@2.1.vendor \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    libbacktrace.vendor \
+    libunwind.vendor
 
 # Fingerprint scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.fingerprint.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.fingerprint.sh \
-    $(LOCAL_PATH)/rootdir/etc/init.fingerprint.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.fingerprint.rc \
-    libbacktrace.vendor \
-    libunwind.vendor
+    $(LOCAL_PATH)/rootdir/etc/init.fingerprint.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.fingerprint.rc
 
 # Fwk-detect
 PRODUCT_PACKAGES += \
