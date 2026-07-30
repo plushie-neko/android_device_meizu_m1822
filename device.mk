ff5b7d7d11e73f183f0d2a18ceba8c0b4659c9b8
plushie-neko <sunflowers.warp@gmail.com>
2026-07-30 04:18:27 +0300

    m1822: Switch to AIDL light HAL
    Kanged from m1721 tree

--- device.mk
+++ device.mk
@@ -263,7 +263,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.meizu_m1822
+    android.hardware.light-service.meizu_m1822
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
